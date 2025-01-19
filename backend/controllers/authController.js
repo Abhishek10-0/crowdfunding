dc24f188eae650a9dbaa2f61c14872bf9cb21acb
abhishekchoudhary <abhishekchoudhary162004@gmail.com>
2025-01-19 09:29:47 +0530

    Half component is done

---
+++ backend/controllers/authController.js
@@ -0,0 +1,38 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+
+const signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found!' });
+    }
+
+   
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid Password' });
+    }
+
+ 
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({
+      message: 'Sign-in successful!',
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        name: user.name,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Something went wrong!' });
+  }
+};
+
+module.exports = { signIn };
