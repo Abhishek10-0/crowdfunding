0dedd1f24834db905b698178c837f92140dd5f73
abhishekchoudhary <abhishekchoudhary162004@gmail.com>
2025-01-19 09:31:53 +0530

    Half component is done

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,23 @@ mongoose
   .catch((err) => console.log('MongoDB connection error:', err));
 
 
+app.post('/api/users/signup', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+ 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully', user: newUser });
+  } catch (error) {
+    res.status(400).json({ error: 'User registration failed', details: error.message });
+  }
+});
+
 
 app.post('/api/users/signin', async (req, res) => {
   console.log('Sign-In request received');
